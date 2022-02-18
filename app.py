a5c144ff7e71d87690afb201907b778c7ddf6848
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2022-02-18 19:12:58 +0530

    uploading latest code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,9 +7,8 @@ import requests
 from bs4 import BeautifulSoup
 import concurrent.futures
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
 def list_books_from_wishlist(link):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(link)
     def load_pages():
         while True:
@@ -43,16 +43,37 @@ def find_amazon_link(book_link):
     else:
         print(book_link, i, "Not working at all........................\n\n\n")
 
+def get_book_detail(amazon_link):
+    data = {}
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get(amazon_link)
+        buttons = driver.find_element_by_xpath('//*[@id="tmmSwatches"]/ul')
+        print([buttons.text], "text\n\n\n")
+        buttons = buttons.find_elements_by_tag_name("li")
+        data['price'] = []
+        for button in buttons:
+            price_list = button.text.split("\n\n")
+            for p in price_list:
+                if len(p)>0:
+                    data['price'].append({p.split("\n")[0]: p.split("\n")[1]})
+        data['title'] = driver.find_element_by_xpath('//*[@id="productTitle"]').text
+        data['product_link'] = driver.current_url
+        data['available'] = ""
+        driver.quit()
+    except Exception as e:
+        print("Product Not Found", e)
+    return data
 
-# final = list_books_from_wishlist("https://www.goodreads.com/review/list/29695889-shreyas?ref=nav_mybooks&shelf=to-read")
-with open("data.txt", "r") as f:
-    final = json.loads(f.read())
+# get_book_detail("https://www.goodreads.com/buy_buttons/14/follow?book_id=56627&page_type=book&page_type_id=56627&ref=x_gr_w_bb_sout&sub_page_type=show&tag=x_gr_w_bb_sout-20%27", '441907')
+
+final = list_books_from_wishlist("https://www.goodreads.com/review/list/80018703-sai-bhaskar-devatha?shelf=to-read")
+
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     result = executor.map(find_amazon_link, final)
-print(list(result))
-# for l in final:
-#     print("\n\n\nBook Link:",l)
-#     print("Amazon Link:", find_amazon_link(l))
 
-# with open("data.txt", "w") as f:
-#     f.write(json.dumps(final))
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    result = executor.map(get_book_detail, list(result))
+
+with open("data.txt", "w") as f:
+   f.write(json.dumps(list(result)))
